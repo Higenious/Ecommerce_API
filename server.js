df278cc2cff39f235b383650899eb4433d4a92da
cchetantayade@gmail.com <cchetantayade@gmail.com>
2020-03-13 23:42:21 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port  = 5000;
+const bodyParser = require('body-parser');
+const log4js = require('log4js');
+const logger = log4js.getLogger();
+logger.level = 'debug';
+logger.debug("server");
+
+
+/** index  */
+app.get('/', (req, res)=>{
+    logger.info('Ecommerce api');
+})
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+/** import all routes */
+logger.info("routes  initializing");
+
+
+/** listen server */
+app.listen(port,()=>{
+    logger.info(`server successfully started running on ${port}`);
+})
